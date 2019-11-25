780f29835c85fa376ddfbff01269243389d5afb0
Yuichiro Katsume <katsume@users.noreply.github.com>
2019-11-26 04:08:02 +0900

    Setup environment

--- gulpfile.js
+++ gulpfile.js
@@ -5,16 +5,13 @@ const	gulp= require('gulp'),
 			plumber= require('gulp-plumber'),
 			named= require('vinyl-named'),
 			webpack= require('webpack'),
-			webpackStream= require('webpack-stream'),
-			// through= require('through2'),
-			ejs= require('gulp-ejs'),
-			sass= require('gulp-sass');
+			webpackStream= require('webpack-stream');
 
 const	src= path.resolve(__dirname, 'src'),
 			dest= path.resolve(__dirname, 'build');
 
 gulp.task('clean', ()=>{
-	return del([path.join(dest, '**', '*.*')]);
+	return del([path.join(dest, 'js', '*.*')]);
 });
 
 gulp.task('webpack', ()=>{
@@ -45,48 +42,8 @@ gulp.task('webpack', ()=>{
 		.pipe(gulp.dest(path.join(dest, 'js')));
 });
 
-// const	appendData= ()=>{
-// 	return through.obj((file, enc, cb)=>{
-//
-// 		file.data= Object.assign(
-// 			{
-// 			},
-// 			JSON.parse(fs.readFileSync(path.resolve(config.path, './ejs/_data.json')))
-// 		);
-//
-// 		cb(null, file);
-// 	});
-// };
-
-gulp.task('ejs', ()=>{
-	return gulp.src([
-			path.join(src, 'ejs', '**', '*.html'),
-			'!'+path.join(src, 'ejs', '**', '_*.ejs')
-		])
-		.pipe(plumber())
-		// .pipe(appendData())
-		.pipe(ejs())
-		.pipe(gulp.dest(dest));
-});
-
-gulp.task('sass', ()=>{
-	return gulp.src(path.join(src, 'sass', '**', '*.scss'))
-		.pipe(sass({
-			outputStyle: (process.env.NODE_ENV==='production')?'compressed':'nested'
-		}))
-		.on('error', (err)=>{
-			console.error('Error', err.message);
-		})
-		.pipe(gulp.dest(path.join(dest, 'css')))
-});
-
-gulp.task('copy', ()=>{
-	return gulp.src(path.join(src, 'www', '**'))
-		.pipe(gulp.dest(dest));
-});
-
 gulp.task('build',
-	gulp.series('clean', gulp.parallel('webpack', 'ejs', 'sass', 'copy'))
+	gulp.series('clean', gulp.parallel('webpack'))
 );
 
 gulp.task('watch', ()=>{
@@ -94,21 +51,6 @@ gulp.task('watch', ()=>{
 	gulp.watch(
 		path.join(src, 'js', '**'),
 		gulp.series('webpack')
-	);
-
-	gulp.watch(
-		path.join(src, 'ejs', '**', '*.(html|ejs|json)'),
-		gulp.series('ejs')
-	);
-
-	gulp.watch(
-		path.join(src, 'sass', '**', '*.scss'),
-		gulp.series('sass')
-	);
-
-	gulp.watch(
-		path.join(src, 'www', '**'),
-		gulp.series('copy')
 	);
 
 });
